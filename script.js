4ec8b5c425ddc658a92817db7a8456b438b4a899
Esteban Bosques <nano.95.eb@gmail.com>
2022-05-11 23:04:11 -0600

    added media controls button and caption button with their respective functionalities

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
-const button = document.getElementById("button");
+const tellJokeBtn = document.getElementById("button-tellJoke");
+const buttonControl = document.getElementById("button-control");
+const buttonCaptions = document.getElementById("button-captions");
+const captionsContainer = document.getElementById("captions-container");
+const captionsWrapper = document.getElementById("captions-wrapper");
+const captionsText = document.getElementById("captions-text");
+const controlIcon = document.getElementById("buttonControl-icon");
 const audioElement = document.getElementById("audio");
 
 const apiUrl =
   "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist";
 
-// Disable/Enable Button
-function toggleButton() {
-  button.disabled = !button.disabled;
+let firstTime = true;
+let captionsOpen = false;
+
+function replaceClass(element, oldValue, newValue) {
+  element.classList.remove(oldValue);
+  element.classList.add(newValue);
+}
+
+// Disable/Enable Buttons
+function toggleButtons() {
+  tellJokeBtn.disabled = !tellJokeBtn.disabled;
+
+  if (tellJokeBtn.disabled) {
+    replaceClass(controlIcon, "fa-play", "fa-pause");
+  } else {
+    replaceClass(controlIcon, "fa-pause", "fa-play");
+  }
+}
+
+// Toggle play/pause control
+function toggleControl() {
+  toggleButtons();
+
+  if (audioElement.paused) {
+    audioElement.play();
+  } else {
+    audioElement.pause();
+  }
+}
+
+// Change captions text
+function updateCaptions(joke) {
+  captionsText.innerHTML = joke;
+  if (captionsOpen) {
+    resizeCaptions(true);
+  }
 }
 
 // Get Jokes from Joke API
 async function getJoke() {
   try {
-    // Disable button
-    toggleButton();
+    // Disable buttons
+    toggleButtons();
+
+    if (firstTime) {
+      buttonControl.disabled = false;
+      buttonCaptions.disabled = false;
+      replaceClass(controlIcon, "fa-play", "fa-pause");
+      firstTime = false;
+    }
 
     // TODO: Add a loader functionality
 
     let joke = "";
+
     const response = await fetch(apiUrl);
     const data = await response.json();
 
@@ -26,6 +73,7 @@ async function getJoke() {
     } else {
       joke = `${data.setup} ... ${data.delivery}`;
     }
+    updateCaptions(joke);
 
     // Text-to-Speech
     tellJoke(joke);
@@ -48,6 +96,23 @@ function tellJoke(joke) {
   });
 }
 
-button.addEventListener("click", getJoke);
+function toggleCaptions() {
+  captionsOpen = !captionsOpen;
+  resizeCaptions(captionsOpen);
+}
 
-audioElement.addEventListener("ended", toggleButton);
+function resizeCaptions(close) {
+  if (captionsOpen) {
+    captionsContainer.style.height = captionsWrapper.clientHeight + "px";
+    captionsContainer.style.borderWidth = "2px";
+  } else {
+    captionsContainer.style.height = 0;
+    captionsContainer.style.borderWidth = "0px";
+  }
+}
+
+tellJokeBtn.addEventListener("click", getJoke);
+buttonControl.addEventListener("click", toggleControl);
+buttonCaptions.addEventListener("click", toggleCaptions);
+
+audioElement.addEventListener("ended", toggleButtons);
